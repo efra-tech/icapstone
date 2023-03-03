a6a7cdd3c22797ca24aa8aec02d1eb690b54a8a0
efra-tech <eahsan@uw.edu>
2023-03-03 14:28:54 -0800

    updated css for navbar

--- react-app/src/components/Nav.jsx
+++ react-app/src/components/Nav.jsx
@@ -6,11 +6,11 @@ export default function Navbar(props) {
     <nav className='navbar-expand-lg'>
       <div>
         <ul className="list-unstyled navbar-nav">
-          <li className='nav-item m-3'><NavLink to="/home">Home</NavLink></li>
-          <li className='nav-item m-3'><NavLink to="/map">Map</NavLink></li>
-          <li className='nav-item m-3'><NavLink to="/events">Events</NavLink></li>
-          <li className='nav-item m-3'><NavLink to="/bulletin">Bulletin</NavLink></li>
-          <li className='nav-item m-3'><NavLink to="/about-us">About Us</NavLink></li>
+          <li className='nav-item m-4'><NavLink to="/home">Home</NavLink></li>
+          <li className='nav-item m-4'><NavLink to="/map">Map</NavLink></li>
+          <li className='nav-item m-4'><NavLink to="/events">Events</NavLink></li>
+          <li className='nav-item m-4'><NavLink to="/bulletin">Bulletin</NavLink></li>
+          <li className='nav-item m-4'><NavLink to="/about-us">About Us</NavLink></li>
         </ul>
       </div>
     </nav>
