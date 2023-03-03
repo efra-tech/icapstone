a335ff30e30313618dbf6d99e476d2e640d1d819
Niha Gaddam <niha@Nihas-MacBook-Air.local>
2023-03-03 15:53:35 -0800

    fix conflict

--- react-app/src/components/AboutPage.jsx
+++ react-app/src/components/AboutPage.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import header from '../imgs/about-us-header.jpeg';
+//import header from '../imgs/about-us-header.jpeg';
 
 export default function AboutPage(props) {
 
 
   return (
     <div>
-      <img src={header} alt={"Rainier Beach P-Patch circa 1980"} />
+      {/* <img src={header} alt={"Rainier Beach P-Patch circa 1980"} /> */}
     </div>
   );
 }
