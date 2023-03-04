7f6624d3db2fc5c8974d9d9e5eee8effcb220f6c
achen77-1575624 <andrew.chen77@yahoo.com>
2023-03-03 16:06:48 -0800

    progress on homepage not really

--- react-app/src/components/HomePage.jsx
+++ react-app/src/components/HomePage.jsx
@@ -4,13 +4,22 @@ export default function HomePage(props) {
   return (
     <div className="homepage">
       <div className="banner">
+      <div style={{ backgroundImage: "url(.\public\imgs\homepage banner.png)" }}>
+        Hello World
+      </div>
         <h1>Garden Space</h1>
       </div>
-      <div className="merry">
+      <div className="carousel">
 
       </div>
       <div className="message">
         
+      </div>
+      <div className="resource">
+
+      </div>
+      <div className="contact">
+
       </div>
     </div>
   );
