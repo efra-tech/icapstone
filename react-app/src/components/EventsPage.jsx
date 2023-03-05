b400d0ac49b0c13d8c9ea4d59ae310c345379c2e
efra-tech <eahsan@uw.edu>
2023-03-05 13:49:45 -0800

    revo-calender css styled

--- react-app/src/components/EventsPage.jsx
+++ react-app/src/components/EventsPage.jsx
@@ -5,55 +5,57 @@ export default function EventsPage(props) {
   return (
     <div>
       <h2>Events Page</h2>
-      <div className="calendar">
-        <
-          RevoCalendar
-          style={{
-            borderRadius: "5px",
-            border: "5px solid #4F6995",
-          }}
-          highlightToday={true}
-          lang="en"
-          primaryColor="#34005A"
-          secondaryColor="#D7E6EE"
-          todayColor="#3B3966"
-          textColor="#333333"
-          indicatorColor="orange"
-          animationSpeed={
-            300
-          }
-          sidebarWidth={
-            180
-          }
-          detailWidth={
-            280
-          }
-          showDetailToggler={
-            true
-          }
-          showSidebarToggler={
-            true
-          }
-          onePanelAtATime={
-            false
-          }
-          allowDeleteEvent={
-            true
-          }
-          allowAddEvent={
-            true
-          }
-          openDetailsOnDateSelection={
-            true
-          }
-          timeFormat24={
-            true
-          }
-          showAllDayLabel={
-            false
-          }
-          detailDateFormat="DD/MM/YYYY"
-        />
+      <div className='calender-container'>
+        <div className="calendar">
+           <
+            RevoCalendar
+            style={{
+              borderRadius: "5px",
+              border: "5px solid #F2F2EC",
+            }}
+            highlightToday={true}
+            lang="en"
+            primaryColor="#829F91"
+            secondaryColor="#F2F2EC"
+            todayColor="#829F91"
+            textColor="#333333"
+            indicatorColor="#3F5749"
+            animationSpeed={
+              300
+            }
+            sidebarWidth={
+              180
+            }
+            detailWidth={
+              280
+            }
+            showDetailToggler={
+              true
+            }
+            showSidebarToggler={
+              true
+            }
+            onePanelAtATime={
+              false
+            }
+            allowDeleteEvent={
+              true
+            }
+            allowAddEvent={
+              true
+            }
+            openDetailsOnDateSelection={
+              true
+            }
+            timeFormat24={
+              true
+            }
+            showAllDayLabel={
+              false
+            }
+            detailDateFormat="DD/MM/YYYY"
+          />
+        </div>
       </div>
     </div>
   );
