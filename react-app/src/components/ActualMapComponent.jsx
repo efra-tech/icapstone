24e8f561707532a78f7b436488fe489b011fb363
achen77-1575624 <andrew.chen77@yahoo.com>
2023-04-07 09:07:41 -0700

    Added unclickable markers

---
+++ react-app/src/components/ActualMapComponent.jsx
@@ -0,0 +1,101 @@
+import React, { useRef, useEffect } from "react";
+import mapboxgl from "mapbox-gl";
+import "./MapPage.css";
+import geoJson from "../data/seattle-sample-data.json";
+import markerImage from "../imgs/marker.png";
+
+mapboxgl.accessToken = process.env.REACT_APP_MAPBOX_TOKEN;
+
+//Somehow route this to side panel
+const Marker = ({ onClick, children, feature }) => {
+    const _onClick = () => {
+      onClick(feature.properties.description);
+    };
+  
+    return (
+      <button onClick={_onClick} className="marker">
+        {children}
+      </button>
+    );
+  };
+
+const ActualMapComponent = () => {
+    const mapContainerRef = useRef(null);
+  
+    // Initialize map when component mounts
+    useEffect(() => {
+      const map = new mapboxgl.Map({
+        container: mapContainerRef.current,
+        style: "mapbox://styles/mapbox/streets-v12",
+        center: [-122.330062, 47.6038321],
+        zoom: 12.5,
+      });
+  
+      map.on("load", function () {
+        // Add an image to use as a custom marker
+        map.loadImage(markerImage,
+          function (error, image) {
+            if (error) {
+                throw error;
+            }
+            map.addImage("custom-marker", image);
+
+            //json file here
+            map.addSource("points", {
+              type: "geojson",
+              data: {
+                type: "FeatureCollection",
+                features: geoJson.features,
+              },
+            });
+            
+            // Add a symbol layer
+            map.addLayer({
+              id: "points",
+              type: "symbol",
+              source: "points",
+              layout: {
+                "icon-image": "custom-marker",
+                // get the title name from the source's "title" property
+                "text-field": ["get", "title"],
+                "text-font": ["Open Sans Semibold", "Arial Unicode MS Bold"],
+                "text-offset": [0, 1.6],
+                "text-anchor": "top",
+              },
+            });
+          }
+        );
+      });
+  
+      // Add navigation control (the +/- zoom buttons)
+      map.addControl(new mapboxgl.NavigationControl(), "top-right");
+  
+      // Clean up on unmount
+      return () => map.remove();
+    }, []);
+  
+    return <div className="map-container" ref={mapContainerRef} />;
+  };
+  
+  export default ActualMapComponent;
+
+
+  // Old Map Component
+//   export default function MapPage(props) {
+//     const mapContainer = useRef(null);
+//     const map = useRef(null);
+//     const [lng, setLng] = useState(-122.330062);
+//     const [lat, setLat] = useState(47.6038321);
+//     const [zoom, setZoom] = useState(12);
+
+//     useEffect(() => {
+//         if (map.current) {
+//             return;
+//         }
+//         map.current = new mapboxgl.Map({
+//             container: mapContainer.current,
+//             style: 'mapbox://styles/mapbox/streets-v12',
+//             center: [lng, lat],
+//             zoom: zoom
+//         });
+//     });
--- react-app/src/components/BulletinPage.jsx
+++ react-app/src/components/BulletinPage.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { useForm } from 'react-hook-form';
+// import { useForm } from 'react-hook-form';
 // import Button from 'react-bootstrap/Button';
 // import Form from 'react-bootstrap/Form';
 import { storage } from '../base';
